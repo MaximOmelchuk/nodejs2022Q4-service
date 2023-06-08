125e1e4116d6564dfdf6b5352a02c864046ab99f
Omelchuk Maxim <omelchukmv@solit.by>
2023-06-08 18:01:22 +0300

    feat: start configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,18 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configService from './ormconfig';
 
 @Module({
-  imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavsModule],
+  imports: [
+    UserModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavsModule,
+    TypeOrmModule.forRoot(configService),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,8 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
 export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   login: string;
+
+  @Column()
   password: string;
+
+  @Column()
   version: number;
+
+  @Column()
   createdAt: number;
+
+  @Column()
   updatedAt: number;
+
+  toResponse() {
+    const { id, login } = this;
+    return { id, login };
+  }
 }
